a0a956a121c4a8a0b9053ee2b08b71f668d55f2f
rohan14shah <rohan14shah@gmail.com>
2024-12-18 09:05:01 -0500

    finish creating columns array

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Arrays;
 
 public class MyPlayer {
     public Chip[][] gameBoard;
@@ -11,6 +12,10 @@ public class MyPlayer {
          * This code will run just once, when the game opens.
          * Add your code here.
          */
+        // fill columns array with 0's
+        for (int i = 0; i < columns.length; i++) {
+            columns[i]=0;
+        }
     }
 
     public Point move(Chip[][] pBoard) {
@@ -29,6 +34,33 @@ public class MyPlayer {
          * Add your code to return the row and the column of the chip you want to take.
          * You'll be returning a data type called Point which consists of two integers.
          */
+
+        // strategy for my player to win :)
+        // basic strategy is that first player wins no matter win if they go first.
+        // the first move is the (1,1) creating an L position
+        // from there all you do is copy the opponents' move so we keep that diagonal symmetry, and we will win no matter what.
+
+        // add code to fill in the columns array
+        // so that it represents the gameBoard in number form
+        // print your columns array to show its working
+
+
+
+        for (int c = 0; c < columns.length; c++) {
+            int count = 0;
+            for (int r = 0; r < gameBoard.length; r++) {
+                if (gameBoard[c][r].isAlive) {
+                    count++;
+
+                }
+
+
+            }
+            columns[c] = count;
+
+        }
+        System.out.println(Arrays.toString(columns));
+
 
         Point myMove = new Point(row, column);
         return myMove;
